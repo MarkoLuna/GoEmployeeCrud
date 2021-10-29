2551eaf0b24e5242d8df9613b4df03cfa44e3d3c
Marcos Luna <josemarcosluna9@gmail.com>
2021-10-28 21:03:13 -0500

    add cors conf and test for validator

--- pkg/app/application.go
+++ pkg/app/application.go
@@ -23,6 +23,7 @@ type Application struct {
 func (app *Application) RegisterRoutes() {
 	routes.RegisterHealthcheckRoute(app.Router)
 	routes.RegisterEmployeeStoreRoutes(app.Router, &app.EmployeeController)
+	enableCORS(app.Router)
 }
 
 func (app *Application) Address() string {
@@ -34,6 +35,24 @@ func (app *Application) Address() string {
 
 func (app *Application) HandleRoutes() {
 	http.Handle("/", app.Router)
+}
+
+func enableCORS(router *mux.Router) {
+	router.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+	}).Methods(http.MethodOptions)
+	router.Use(middlewareCors)
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, req *http.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+			next.ServeHTTP(w, req)
+		})
 }
 
 func (app *Application) StartServer() {
---
+++ pkg/utils/validator_test.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"testing"
+	"time"
+
+	"github.com/MarkoLuna/GoEmployeeCrud/pkg/constants"
+	"github.com/MarkoLuna/GoEmployeeCrud/pkg/models"
+)
+
+func TestValidEmployee(t *testing.T) {
+
+	var employee models.Employee
+	employee.FirstName = "Marcos"
+	employee.LastName = "Luna"
+	employee.SecondLastName = "Valdez"
+	employee.DateOfBirth = time.Date(1994, time.April, 25, 8, 0, 0, 0, time.UTC)
+	employee.DateOfEmployment = time.Now().UTC()
+	employee.Status = constants.ACTIVE
+
+	v := CreateValidator()
+	err1 := v.Struct(employee)
+	if err1 != nil {
+		t.Error("the employee should be valid")
+	}
+}
+
+func TestParseEmployeeWithInvalidData(t *testing.T) {
+
+	employee := &models.Employee{}
+
+	v := CreateValidator()
+	err1 := v.Struct(employee)
+	if err1 == nil {
+		t.Error("the body should not be valid")
+	}
+}
