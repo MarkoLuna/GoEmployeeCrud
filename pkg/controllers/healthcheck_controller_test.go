8b34a2880a7133808488f04361e35f61d3a99277
Marcos Luna <josemarcosluna9@gmail.com>
2022-04-01 15:18:48 -0600

    update unit tests for healthcheck controller

--- pkg/controllers/healthcheck_controller_test.go
+++ pkg/controllers/healthcheck_controller_test.go
@@ -4,27 +4,31 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestHealthCheckHandler(t *testing.T) {
+	e := echo.New()
 	req, err := http.NewRequest("GET", "/healthcheck/", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(HealthCheckHandler)
+	c := e.NewContext(req, rr)
 
-	handler.ServeHTTP(rr, req)
+	if assert.NoError(t, HealthCheckHandler(c)) {
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusOK)
+		}
 
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-
-	expected := `OK`
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
+		expected := `OK`
+		if rr.Body.String() != expected {
+			t.Errorf("handler returned unexpected body: got %v want %v",
+				rr.Body.String(), expected)
+		}
 	}
 }
